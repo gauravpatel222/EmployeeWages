dc8e1bb4a8fa61920c393112f94c5e72efd5db74
Gaurav <gauravgurjar8791@gmail.com>
2025-03-21 10:53:30 +0530

    Check emp

---
+++ Employeewage.js
@@ -0,0 +1,12 @@
+//UC-1 checking 
+const present = 1;
+
+let isPresent = Math.floor(Math.random()*10)%2;
+
+if(isPresent == present){
+    console.log("Employee is Present");
+}
+else{
+    console.log("Employee is not Present");
+    return;
+}
