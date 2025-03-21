6c60e93c1b6de3b1689d95491d7b20ca804809d9
Gaurav <gauravgurjar8791@gmail.com>
2025-03-21 10:54:51 +0530

    Daily wage

--- Employeewage.js
+++ Employeewage.js
@@ -10,3 +10,31 @@ else{
     console.log("Employee is not Present");
     return;
 }
+
+//UC-2 and UC-3 calculating dailywage of an employee with using a function
+let workingHours = Math.floor(Math.random()*10)%3;
+
+const isPartTime = 1;
+const isFullTime = 2;
+const partTime = 4;
+const fullTime = 8;
+const wagePerHour = 20;
+
+function getWorkingHours(workingHours){
+    switch(workingHours){
+
+        case isPartTime:
+            return partTime;
+
+        case isFullTime:
+            return fullTime;
+
+        default :
+            return 0;
+
+    }
+}
+
+let wage = wagePerHour*getWorkingHours(workingHours);
+
+console.log("Daily wage generated for employee is ",wage);
