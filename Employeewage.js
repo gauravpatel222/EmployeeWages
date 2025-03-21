0768ed72fc29513e473c489a04edeb83bda39067
Gaurav <gauravgurjar8791@gmail.com>
2025-03-21 11:06:25 +0530

    wags till conditon

--- Employeewage.js
+++ Employeewage.js
@@ -53,3 +53,49 @@ for(let i = 1; i<=numberOfWorkingDays;i++){
 let empWage = empHrs*wagePerHour;
 console.log("Monthly Wage of the Employee for hours "+empHrs+" is "+empWage);
 
+
+//UC-5 calculating Employee wage Untill hours are 160 or days completed are 20 
+
+empHrs = 0;
+let numberOfTotalDays = 0;
+while(empHrs <= 160 && numberOfTotalDays < 20){
+    
+    empCheck = Math.floor(Math.random()*10)%3;
+    empHrs += getWorkingHours(empCheck);
+    numberOfTotalDays++;
+
+}
+empWage = empHrs*wagePerHour; 
+
+console.log("Total working days are : "+numberOfTotalDays+", Total working hours are : "+empHrs+", Wage of Employee is : "+empWage);
+
+//UC-6 storing the daily wage in an array and also calculating totalWage
+
+function getWage(empHrs){
+    return empHrs*wagePerHour;
+}
+
+const maxWorkingHrs = 160;
+const maxWorkingDays = 20;
+
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+let dailyWages = new Array();
+
+numberOfTotalDays = 0;
+
+while(totalEmpHrs <= maxWorkingHrs && numberOfTotalDays < maxWorkingDays){
+    
+    empCheck = Math.floor(Math.random()*10)%3;
+    empHrs = getWorkingHours(empCheck);
+
+    totalEmpHrs += empHrs;
+
+    dailyWages.push(getWage(empHrs));
+
+    numberOfTotalDays++;
+
+}
+
+empWage = totalEmpHrs*wagePerHour;
+console.log("Total working days are : "+numberOfTotalDays+", Total working hours are : "+totalEmpHrs+", Wage of Employee is : "+empWage);
